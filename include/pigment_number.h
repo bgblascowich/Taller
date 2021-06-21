20d76160b1f192e93151ab986d2d66058fa1d06a
Silvia Pabon <silvia.pabon.2020@upb.edu.co>
2021-06-21 14:51:54 -0500

    Add function pigment_numbers
    -Add a function that prints a message of numbers in black color

---
+++ include/pigment_number.h
@@ -0,0 +1,10 @@
+#ifndef TALLER_PIGMENT_NUMBER_H
+#define TALLER_PIGMENT_NUMBER_H
+
+#include <string>
+
+namespace pigment_numbers {
+    std::string message(std::string message);
+}
+
+#endif //TALLER_PIGMENT_NUMBER_H
--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 #include "pigment.h"
 #include "print_screen.h"
+#include "pigment_number.h"
 
 int main() {
     std::cout << pigments::red("This is red!") << std::endl;
     std::cout << pigments::green("This is green!") << std::endl;
+    std::cout << pigment_numbers::message("This is a black message of 1234!") << std::endl;
     print_screen::red("From print_screen::red");
     print_screen::green("From print_screen::green");
     return 0;
